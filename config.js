8c37ea80af9637b63408b7856ff30f88e7bf861e
Ranabrand36 <mirshadm802@gmail.com>
2026-07-29 22:26:26 +0500

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,8 @@ module.exports = {
   botName: process.env.BOT_NAME || 'LEGEND MD BOT',
   ownerName: process.env.OWNER_NAME || 'Legend Hassan',
   ownerNumber: process.env.OWNER_NUMBER || '923039507788',
+  channelName: 'Legend Hassan',
+  channelLink: 'https://whatsapp.com/channel/0029Vb7VcqlBlHpdhAL7f80S',
+  channelId: '120363407511472969@newsletter',
   prefix: '.',
 };
