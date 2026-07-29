9d410c6daea31a85eaf01c70f2eecd479a57cba1
Ranabrand36 <mirshadm802@gmail.com>
2026-07-29 22:39:58 +0500

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,7 +1,9 @@
-cat > utils.js << "EOF"
 function getChannelFooter(config) {
-  return `\n\n📢 *${config.channelName}*\n┗━〖 ${config.channelLink} 〗`;
+  // Agar config mein values nahi bhi hon toh yeh fallback kaam karega
+  const name = config?.channelName || 'Legend Hassan';
+  const link = config?.channelLink || 'https://whatsapp.com/channel/0029Vb7VcqlBlHpdhAL7f80S';
+  
+  return `\n\n📢 *${name}*\n┗━〖 ${link} 〗`;
 }
 
 module.exports = { getChannelFooter };
-EOF
