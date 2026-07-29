6d33de3c49dbd58f39515841b41f37659856f961
Ranabrand36 <mirshadm802@gmail.com>
2026-07-29 22:44:04 +0500

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,7 +1,7 @@
 function getChannelFooter(config) {
-  // Agar config mein values nahi bhi hon toh yeh fallback kaam karega
-  const name = config?.channelName || 'Legend Hassan';
-  const link = config?.channelLink || 'https://whatsapp.com/channel/0029Vb7VcqlBlHpdhAL7f80S';
+  // Agar config mein values na bhi hon toh yeh fallback kaam karega
+  const name = (config && config.channelName) ? config.channelName : 'Legend Hassan';
+  const link = (config && config.channelLink) ? config.channelLink : 'https://whatsapp.com/channel/0029Vb7VcqlBlHpdhAL7f80S';
   
   return `\n\n📢 *${name}*\n┗━〖 ${link} 〗`;
 }
