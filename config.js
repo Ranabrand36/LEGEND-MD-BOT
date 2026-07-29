5c982b52beca9abe81b9df6adce2106ebe7806db
Ranabrand36 <mirshadm802@gmail.com>
2026-07-29 10:23:21 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+
+module.exports = {
+  botName: process.env.BOT_NAME || 'LEGEND MD BOT',
+  ownerName: process.env.OWNER_NAME || 'Legend Hassan',
+  ownerNumber: process.env.OWNER_NUMBER || '923039507788',
+  prefix: '.',
+};
