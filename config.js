2da1dfedbe7554f1cac9db0bab46b8f2b5e50582
Ranabrand36 <mirshadm802@gmail.com>
2026-07-29 22:39:31 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,15 @@
-cat > config.js << "EOF"
 require('dotenv').config();
 
 module.exports = {
   botName: process.env.BOT_NAME || 'LEGEND MD BOT',
   ownerName: process.env.OWNER_NAME || 'Legend Hassan',
   ownerNumber: process.env.OWNER_NUMBER || '923039507788',
+  
+  // ===== YEH 2 LINES ZAROOR HONI CHAHIYE (Maine neeche hardcode bhi kar diya hai) =====
   channelName: 'Legend Hassan',
   channelLink: 'https://whatsapp.com/channel/0029Vb7VcqlBlHpdhAL7f80S',
   channelId: '120363407511472969@newsletter',
+  // ======================================================================
+  
   prefix: '.',
 };
-EOF
