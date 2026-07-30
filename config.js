16a1ccffea010e19667a3e2ed6d9c6e304578ca0
Ranabrand36 <mirshadm802@gmail.com>
2026-07-30 10:12:56 +0500

    Update config.js

--- config.js
+++ config.js
@@ -4,12 +4,8 @@ module.exports = {
   botName: 'LEGEND MD BOT',
   ownerName: 'Legend Hassan',
   ownerNumber: '923039507788',
-  
-  // ===== YAHAN CHANNEL KI VALUES DIRECT DAAL DI HAIN =====
   channelName: 'Legend Hassan',
   channelLink: 'https://whatsapp.com/channel/0029Vb7VcqlBlHpdhAL7f80S',
   channelId: '120363407511472969@newsletter',
-  // ======================================================
-  
   prefix: '.',
 };
